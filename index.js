ba73f167d8f5ace9952d6d9416d3ca0588f9c130
Kaka <kakalition@gmail.com>
2024-06-02 16:08:58 +0700

    patch

--- index.js
+++ index.js
@@ -82,7 +82,6 @@ app.delete('/applications/:uuid', async (req, res) => {
 });
 
 // Menus
-
 app.get('/applications/:uuid/menus', async (req, res) => {
   const instance = await mongoDb.getInstance();
   const collection = await instance.collection('menus');
@@ -109,39 +108,86 @@ app.post('/applications/:uuid/menus', async (req, res) => {
   res.send(result).status(201);
 });
 
-// app.put('/applications/:uuid', async (req, res) => {
-//   const instance = await mongoDb.getInstance();
-//   const collection = await instance.collection('application');
+app.put('/applications/:uuid/menus/:menuUuid', async (req, res) => {
+  const instance = await mongoDb.getInstance();
+  const collection = await instance.collection('menus');
 
-//   const currentItem = await collection.findOne({
-//     uuid: req.params['uuid']
-//   })
+  const currentItem = await collection.findOne({
+    uuid: req.params['menuUuid']
+  })
 
-//   currentItem.name = req.body.name;
+  currentItem.name = req.body.name;
+  currentItem.index = req.body.index;
 
-//   const result = await collection.updateOne(
-//     {
-//       uuid: currentItem.uuid
-//     },
-//     {
-//       "$set": currentItem,
-//     }
-//   );
+  const result = await collection.updateOne(
+    {
+      uuid: currentItem.uuid
+    },
+    {
+      "$set": currentItem,
+    }
+  );
 
-//   res.send(result).status(200);
-// });
+  res.send(result).status(200);
+});
 
-// app.delete('/applications/:uuid', async (req, res) => {
-//   const instance = await mongoDb.getInstance();
-//   const collection = await instance.collection('application');
+app.delete('/applications/:uuid/menus/:menuUuid', async (req, res) => {
+  const instance = await mongoDb.getInstance();
+  const collection = await instance.collection('menus');
 
-//   const result = await collection.deleteOne({
-//     uuid: req.params['uuid']
-//   })
+  const result = await collection.deleteOne({
+    uuid: req.params['menuUuid']
+  })
 
-//   res.send(result).status(200);
-// });
+  res.send(result).status(200);
+});
 
+// Quiz
+app.get('/applications/:uuid/menus/:menuUuid/quiz', async (req, res) => {
+  const instance = await mongoDb.getInstance();
+  const collection = await instance.collection('quiz');
+
+  const result = await collection.findOne({
+    menuUuid: req.params.menuUuid
+  });
+
+  res.send(result ?? { quiz: [] }).status(200);
+});
+
+app.post('/applications/:uuid/menus/:menuUuid/quiz', async (req, res) => {
+  const instance = await mongoDb.getInstance();
+  const collection = await instance.collection('quiz');
+
+  const result = await collection.insertOne({
+    uuid: uuid(),
+    appUuid: req.params.uuid,
+    ...req.body,
+  });
+
+  res.send(result).status(200);
+});
+
+app.put('/applications/:uuid/menus/:menuUuid/quiz/:quizUuid', async (req, res) => {
+  const instance = await mongoDb.getInstance();
+  const collection = await instance.collection('quiz');
+
+  const currentItem = await collection.findOne({
+    uuid: req.params['quizUuid']
+  })
+
+  const result = await collection.updateOne(
+    {
+      uuid: currentItem.uuid
+    },
+    {
+      "$set": {
+        quiz: req.body.quiz
+      },
+    }
+  );
+
+  res.send(result).status(200);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
